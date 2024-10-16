91d1069b8505ba3eae8aa14aa967381eae52e806
jinhyeok0204 <aqwstn@naver.com>
2024-10-16 22:47:29 +0900

    important info hiding from config.py

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+from dotenv import load_dotenv
 
 # Application, DB 초기화
 
+load_dotenv()
 app = Flask(__name__)
 app.config.from_object(Config)
-app.secret_key = "secretkey"
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
